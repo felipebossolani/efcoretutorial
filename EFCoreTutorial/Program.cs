25f679f3353b37797cdf02475f90b37def83f83a
felipebossolani <fbossolani@gmail.com>
2018-12-24 12:49:23 -0200

    First Commit

--- EFCoreTutorial/Program.cs
+++ EFCoreTutorial/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EFCoreTutorial.Models;
+using System;
+
 
 namespace EFCoreTutorial
 {
@@ -6,7 +8,48 @@ namespace EFCoreTutorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is my first EFCore Application:");
+           
+            using(var context = new SchoolDBContext())
+            {
+                Console.WriteLine("Adding course...");
+
+                var testCourse = new Course()
+                {
+                    CourseId = 9999,
+                    Title = "EFCore Tutorial",
+                    Credits = 9,
+                    DepartmentId = 2
+                };
+
+                context.Course.Add(testCourse);
+                context.SaveChanges();
+                Console.WriteLine("Course added");
+
+                var courses = context.Course;
+
+                Console.WriteLine("Listing all courses in Title Order (ASC)");
+                foreach(var course in courses)
+                {
+                    Console.WriteLine($"ID: {course.CourseId}, Title: {course.Title}");
+                }
+
+                Console.WriteLine("Deleting CourseId 9999");
+                context.Course.Remove(testCourse);
+
+                context.SaveChanges();
+                Console.WriteLine("Course removed");
+
+                courses = context.Course;
+
+                Console.WriteLine("Listing all courses in Title Order (ASC)");
+
+                Console.WriteLine("Listing all courses in Title Order (ASC)");
+                foreach (var course in courses)
+                {
+                    Console.WriteLine($"ID: {course.CourseId}, Title: {course.Title}");
+                }
+            }
         }
     }
 }
---
+++ EFCoreTutorial/readme.md
@@ -0,0 +1,1 @@
+﻿This is my first EF Core project. I used SchoolDB provided by Microsoft and this tutorial: http://www.entityframeworktutorial.net/efcore/entity-framework-core-dbcontext.aspx
